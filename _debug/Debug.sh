5c665fad4343708693aeaca657fda37b1eb18835
Jyph <galvja2006@gmail.com>
2025-05-10 01:38:05 -0500

    Fixed the Icon folder in the zip

--- _debug/Debug.sh
+++ _debug/Debug.sh
@@ -5,8 +5,10 @@ jar cfve RandomChampion.jar RandomChampion *.class
 cd ..
 mv _build/RandomChampion.jar RANDOM_CHAMPION/RandomChampion.jar
 
-zip RandomChampion.zip RANDOM_CHAMPION/*
-tar zcvf RandomChampion.tar.gz RANDOM_CHAMPION/*
+zip -q -r RandomChampion.zip RANDOM_CHAMPION/*
+tar qzcvf RandomChampion.tar.gz RANDOM_CHAMPION/*
+
+unzip -q RandomChampion.zip -d _debug/_unzip
 
 cd RANDOM_CHAMPION
 java -jar RandomChampion.jar
